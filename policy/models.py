edbc1177962b5b4867b5ff2a9a1e3b2b5b83116f
student <oumagladwineesther@gmail.com>
2025-09-27 09:44:12 +0300

    Malipoflex backup

---
+++ policy/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Policy(models.Model):   
+    title = models.CharField(max_length=100)     
+    description = models.TextField() 
+    effective_date = models.DateTimeField()  
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"{self.title} Policy ({self.id})"
+
